d224e60341aa38e8381e8b22fd09f350dd809c2a
or.maymon <ormay30@gmail.com>
2023-07-10 14:05:28 +0300

    fixed the tab component

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildDecorator, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabs-container.component.css']
 })
 export class TabsContainerComponent {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
+
+  constructor() {
+  }
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active)
+
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => { tab.active = false })
+
+    tab.active = true;
+
+    return false
+  }
 
 }
