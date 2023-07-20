4073bde789b54ed5fad846e2cbe4f55ae2e98d3c
or.maymon <ormay30@gmail.com>
2023-07-20 16:36:23 +0300

    imported firebase and inmstalled angularFire

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCO257VSBIvYh6Ts5eaypqKaNz8Fs9FVPQ",
+        authDomain: "clips-afca2.firebaseapp.com",
+        projectId: "clips-afca2",
+        storageBucket: "clips-afca2.appspot.com",
+        appId: "1:410320549379:web:bf6b79a1ea3818b0bc8036"
+    },
+    production: false,
+};
