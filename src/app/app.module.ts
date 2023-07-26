d84de4fcc5677319c0fa2402758404e097d39b7d
or.maymon <ormay30@gmail.com>
2023-07-26 18:53:56 +0300

    fixed bug with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,12 +26,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     PictureModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
